38a1500971e00f5f8e9118517a7d4a900c1b4e34
yoyi-88 <ygomben2510@g.educaand.es>
2026-04-20 11:52:08 +0200

    Seguridad XSS

--- src/app/detalle/detalle.ts
+++ src/app/detalle/detalle.ts
@@ -12,8 +12,26 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class Detalle implements OnInit {
   rutaActiva = inject(ActivatedRoute);
+
+  // Inyectamos el Sanitizador 
+  sanitizador = inject(DomSanitizer);
+
   idMoneda = signal<string>('');
 
+  // --- SIMULACIÓN DE DATOS EXTERNOS ---
+
+  // Un texto que simula venir de una base de datos atacada por un hacker
+  noticiaHacker = `
+    <p>El precio de esta moneda va a subir a la luna 🚀</p>
+    <script>alert('¡PUM! Te he robado las contraseñas.');</script>
+    <a href="javascript:alert('¡Hackeado!')" style="color: red;">Haz clic aquí para ganar 1000€</a>
+  `;
+
+  // Una URL de un iframe de YouTube. Angular la bloqueará por defecto.
+  urlVideoOriginal = 'https://www.youtube.com/embed/dQw4w9WgXcQ';
+  
+  // Usamos un tipo especial de Angular (SafeResourceUrl) para decirle al HTML que confíe
+  urlVideoSegura = signal<SafeResourceUrl | null>(null);
 
   ngOnInit() {
     const idDeLaUrl = this.rutaActiva.snapshot.paramMap.get('id');
@@ -22,6 +40,13 @@ export class Detalle implements OnInit {
     if (idDeLaUrl) {
       this.idMoneda.set(idDeLaUrl);
     }
+
+    // BYPASS DE SEGURIDAD (El Botón de Confianza)
+    // Le decimos al guardia: "Yo asumo la responsabilidad de este enlace, déjalo pasar".
+    const enlaceVerificado = this.sanitizador.bypassSecurityTrustResourceUrl(this.urlVideoOriginal);
+    
+    // Guardamos el enlace seguro en el Signal
+    this.urlVideoSegura.set(enlaceVerificado);
   }
 
 }
